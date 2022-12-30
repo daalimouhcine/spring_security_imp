8a8b6255bef89f702c57912f72fcbc62b0cd59f5
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-30 10:02:07 +0100

    build(project): initialize the project with spring initializer

---
+++ src/test/java/com/ex/spring_security_imp/SpringSecurityImpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.spring_security_imp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityImpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
