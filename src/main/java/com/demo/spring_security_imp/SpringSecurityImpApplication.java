f409327a403d1f7da47a45bf5a9c51fc4b40896d
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-01-02 16:10:31 +0100

    build(project): add authUsers and passwordEncoder methods to Autowrite theme

--- src/main/java/com/demo/spring_security_imp/SpringSecurityImpApplication.java
+++ src/main/java/com/demo/spring_security_imp/SpringSecurityImpApplication.java
@@ -1,13 +1,27 @@
 package com.demo.spring_security_imp;
 
+import com.demo.spring_security_imp.DATA.AuthUsers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringSecurityImpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityImpApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthUsers authUsers() {
+        return new AuthUsers();
     }
 
 }
