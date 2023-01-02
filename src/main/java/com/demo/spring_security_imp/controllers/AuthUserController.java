d9f87640e57cc1f041ae6e133305c0e678dd3d3e
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-01-02 16:07:46 +0100

    build(project): create user auth controller and create the register and get all methods

---
+++ src/main/java/com/demo/spring_security_imp/controllers/AuthUserController.java
@@ -0,0 +1,43 @@
+package com.demo.spring_security_imp.controllers;
+
+import com.demo.spring_security_imp.requests.UserRequest;
+import com.demo.spring_security_imp.responses.UserResponse;
+import com.demo.spring_security_imp.services.AuthUserService;
+import com.demo.spring_security_imp.shaired.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("api")
+public class AuthUserController {
+    @Autowired
+    private AuthUserService authUserService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest) {
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userRequest, userDto);
+        UserDto createdUser = authUserService.registerUser(userDto);
+        UserResponse userResponse = new UserResponse();
+        BeanUtils.copyProperties(createdUser, userResponse);
+        return new ResponseEntity<UserResponse>(userResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<ArrayList<UserResponse>> getAllUsers() {
+        ArrayList<UserDto> userDtos = authUserService.getAllUsers();
+        ArrayList<UserResponse> userResponses = new ArrayList<>();
+        for (UserDto userDto : userDtos) {
+            UserResponse userResponse = new UserResponse();
+            BeanUtils.copyProperties(userDto, userResponse);
+            userResponses.add(userResponse);
+        }
+        return new ResponseEntity<ArrayList<UserResponse>>(userResponses, HttpStatus.OK);
+    }
+}
