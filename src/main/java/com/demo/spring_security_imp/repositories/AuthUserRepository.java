f9ff0fc6d492f6ea9709aa4b0be8f0c66b753a76
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-01-02 16:07:56 +0100

    build(project): create user auth repository

---
+++ src/main/java/com/demo/spring_security_imp/repositories/AuthUserRepository.java
@@ -0,0 +1,11 @@
+//package com.demo.spring_security_imp.repositories;
+//
+//import com.demo.spring_security_imp.DATA.User;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface AuthUserRepository extends CrudRepository<User, Long> {
+//    User findByUsername(String username);
+//}
+
