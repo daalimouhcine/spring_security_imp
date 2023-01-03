17d6c5dbd7b9bd3010fd1437d808f8848b9eb65e
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-01-03 22:46:52 +0100

    build(project): create security config file

--- src/main/java/com/demo/spring_security_imp/security/SecurityConfig.java
+++ src/main/java/com/demo/spring_security_imp/security/SecurityConfig.java
@@ -1,10 +1,84 @@
 package com.demo.spring_security_imp.security;
 
+import com.demo.spring_security_imp.services.AuthUserService;
+import com.demo.spring_security_imp.shaired.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-        //@EnableWebSecurity
-        //@Configuration
-        //public class SecurityConfig {
-        //
-        //}
+import java.util.Collections;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private AuthUserService authUserService;
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/**", "test")
+                .permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/user/**").hasAnyAuthority("USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .authenticationProvider()
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+                UserDto userDto = authUserService.findByEmail(email);
+                if(userDto == null) {
+                    return null;
+                }
+                UserDetails userAuth = new User(userDto.getEmail(), userDto.getPassword(), Collections.singleton(new SimpleGrantedAuthority("USER")));
+
+                return userAuth;
+            }
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
